bfb13dedbae6e564e3a0b1d3b82690e0645855db
Egor Podolskiy <eapodolskiy@avito.ru>
2024-10-06 22:40:30 +0300

    SC-3 add react-router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainPage } from '../pages/main-page';
+import { LoginPage } from '../pages/login-page';
+import { FavoritesPage } from '../pages/favorites-page';
+import { OfferPage } from '../pages/offer-page';
+import { PrivateRoute } from '../shared/routes/private-route';
+import { Page404 } from '../pages/errors';
+import { AppRoutes } from './routes';
 
 const OFFERS_AMOUNT = 555;
 
 export const App = () => (
-	<MainPage offersAmount={OFFERS_AMOUNT} />
+	<BrowserRouter>
+		<Routes>
+			<Route path={AppRoutes.Default}>
+				<Route index element={<MainPage offersAmount={OFFERS_AMOUNT} />} />
+				<Route path={AppRoutes.Login} element={<LoginPage />} />
+				<Route path={AppRoutes.Favorites} element={(
+					<PrivateRoute isAuthenticated={false}>
+						<FavoritesPage />
+					</PrivateRoute>
+				)}
+				/>
+				<Route path={AppRoutes.Offer} element={<OfferPage />} />
+			</Route>
+			<Route path='*' element={<Page404 />} />
+		</Routes>
+	</BrowserRouter>
 );
--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import { OfferCard } from '../../components';
+import { AppRoutes } from '../../app/routes';
 
 type Props = {
 	offersAmount: number;
@@ -20,12 +22,12 @@ export const MainPage = (props: Props) => {
 						<nav className="header__nav">
 							<ul className="header__nav-list">
 								<li className="header__nav-item user">
-									<a className="header__nav-link header__nav-link--profile" href="#">
+									<Link className="header__nav-link header__nav-link--profile" to={AppRoutes.Favorites}>
 										<div className="header__avatar-wrapper user__avatar-wrapper">
 										</div>
 										<span className="header__user-name user__name">Oliver.conner@gmail.com</span>
 										<span className="header__favorite-count">3</span>
-									</a>
+									</Link>
 								</li>
 								<li className="header__nav-item">
 									<a className="header__nav-link" href="#">
