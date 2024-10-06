0115f99f4651a556be821fd0a47f3a0275c20e79
Egor Podolskiy <eapodolskiy@avito.ru>
2024-10-06 22:40:30 +0300

    SC-3 add react-router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainPage } from '../pages/main-page';
+import { LoginPage } from '../pages/login-page';
+import { FavoritesPage } from '../pages/favorites-page';
+import { OfferPage } from '../pages/offer-page';
+import { PrivateRoute } from '../shared/routes/private-route';
+import { Page404 } from '../pages/errors';
 
 const OFFERS_AMOUNT = 555;
 
 export const App = () => (
-	<MainPage offersAmount={OFFERS_AMOUNT} />
+	<BrowserRouter>
+		<Routes>
+			<Route path='/'>
+				<Route index element={<MainPage offersAmount={OFFERS_AMOUNT} />} />
+				<Route path='login' element={<LoginPage />} />
+				<Route path='favorites' element={(
+					<PrivateRoute isAuthenticated={false}>
+						<FavoritesPage />
+					</PrivateRoute>
+				)}
+				/>
+				<Route path='offer/:id' element={<OfferPage />} />
+			</Route>
+			<Route path='*' element={<Page404 />} />
+		</Routes>
+	</BrowserRouter>
 );
